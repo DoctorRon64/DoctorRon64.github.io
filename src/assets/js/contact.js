416a815add07096c5d99af76cc2efed469cd215e
DrRon <DoctorRon64@users.noreply.github.com>
2024-12-24 01:22:19 +0100

    Contact info changed

--- src/assets/js/contact.js
+++ src/assets/js/contact.js
@@ -11,9 +11,9 @@ function createFooter(addSocialClass) {
     ul.className = 'social-icons';
 
     const socialLinks = [
-        { href: 'mailto:philip@lefthandmedia.com', title: 'Email me', imgSrc: '/assets/img/icons/email.svg', alt: 'Email Icon', widht:'150', height:'150' },
+        { href: 'mailto:y2t1l3mt5@mozmail.com', title: 'Email me', imgSrc: '/assets/img/icons/email.svg', alt: 'Email Icon', widht:'150', height:'150' },
         { href: 'https://github.com/DoctorRon64', title: 'GitHub', imgSrc: '/assets/img/icons/skill-icons--github-dark.svg', alt: 'GitHub Icon', target: '_blank' },
-        { href: 'https://www.linkedin.com/in/philip-de-groot-84272130b/', title: 'LinkedIn', imgSrc: '/assets/img/icons/devicon--linkedin.svg', alt: 'LinkedIn Icon', target: '_blank' },
+        { href: 'www.linkedin.com/in/gamedev-gamecomposer-84272130b', title: 'LinkedIn', imgSrc: '/assets/img/icons/devicon--linkedin.svg', alt: 'LinkedIn Icon', target: '_blank' },
         { href: 'https://open.spotify.com/artist/6szCm5FAd4pBlQw6US2DPw?si=tF8kTzDnS-mFpSLxSeZKBQ', title: 'Spotify', imgSrc: '/assets/img/icons/logos--spotify-icon.svg', alt: 'Spotify Icon', target: '_blank' },
         { href: 'https://doctor-ron.itch.io/', title: 'Itch.Io', imgSrc: '/assets/img/icons/cib--itch-io.svg', alt: 'Itch.io Icon', target: '_blank', style: 'fill: #fa5c5c;' }
     ];
